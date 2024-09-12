e08fe27b089d54ea1bfbe3f241f197a41a7e9acc
NAYAN_SONI <nayansoni142004@gmail.com>
2024-09-12 15:32:25 +0530

    context obj. entire application

---
+++ 26_init_param/WEB-INF/src/servlets/ProcessServlet.java
@@ -0,0 +1,30 @@
+package servlets;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+
+public class ProcessServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        ServletConfig conf = getServletConfig();
+        ServletContext context = conf.getServletContext();
+        String aaa = context.getInitParameter("aaa");
+
+        response.setContentType("text/html");
+
+        PrintWriter pw = response.getWriter();
+
+        pw.write("<h1>" + aaa + "</h1>");
+
+        pw.flush();
+        pw.close();
+    }
+}
