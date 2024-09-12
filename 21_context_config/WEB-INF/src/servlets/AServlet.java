ad86d87b160d6a2176851d4df92e386ad6145429
NAYAN_SONI <nayansoni142004@gmail.com>
2024-09-12 15:15:30 +0530

    context-config 1 time  initialization

---
+++ 21_context_config/WEB-INF/src/servlets/AServlet.java
@@ -0,0 +1,36 @@
+package servlets;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+
+@WebServlet("/a.do")
+public class AServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+        response.setContentType("text/html");
+
+        PrintWriter pw = response.getWriter();
+
+        ServletConfig conf = getServletConfig();
+
+        //ServletContext context = conf.getServletContext(); //can be called directly...
+        ServletContext context = getServletContext();
+
+        pw.write("<h1>-" + conf + "</h1>");
+        pw.write("<h1>-" + context + "</h1>");
+        pw.write("<h1>-" + request + "</h1>");
+        pw.write("<h1>-" + response + "</h1>");
+
+        pw.flush();
+        pw.close();
+    }
+}
