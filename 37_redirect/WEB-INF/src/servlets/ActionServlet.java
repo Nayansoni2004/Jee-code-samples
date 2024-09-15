1e0aa24eaff819974ad064da2a48cad57fd01f6a
NAYAN_SONI <nayansoni142004@gmail.com>
2024-09-15 22:47:45 +0530

    new req. URL sendRedirect

---
+++ 37_redirect/WEB-INF/src/servlets/ActionServlet.java
@@ -0,0 +1,25 @@
+package servlets;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+
+import java.io.IOException;
+
+@WebServlet("/act.do")
+public class ActionServlet extends HttpServlet {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String userName = request.getParameter("nm");
+        String email = request.getParameter("em");
+        String password = request.getParameter("pw");
+
+        System.out.println("Action Servlet ************POST************start");
+        System.out.println(userName + " - " + email + " - " + password);
+        System.out.println("Action Servlet ************POST**************end");
+
+        //request.getRequestDispatcher("pro.do").forward(request, response);
+        response.sendRedirect("pro.do");
+    }
+}
