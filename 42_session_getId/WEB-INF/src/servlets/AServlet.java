9ca52e0cf1ad001e83a2f2abc15727f1bf790a2e
NAYAN_SONI <nayansoni142004@gmail.com>
2024-09-20 00:29:16 +0530

    one session obj. per clients

---
+++ 42_session_getId/WEB-INF/src/servlets/AServlet.java
@@ -0,0 +1,23 @@
+package servlets;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.ServletException;
+
+import java.io.IOException;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/a.do")
+public class AServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        HttpSession session = request.getSession();
+
+        String sessionId = session.getId();
+
+        System.out.println("AServlet *************SessionID: " + sessionId);
+
+        request.getRequestDispatcher("pro.html").forward(request, response);
+    }
+}
