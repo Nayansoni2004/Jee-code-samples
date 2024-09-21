dd54fa08ebd36d7659286fa697e336ebfc90d102
NAYAN_SONI <nayansoni142004@gmail.com>
2024-09-21 23:34:34 +0530

    one session multiple servlets working

---
+++ 43_session_isNew/WEB-INF/src/servlets/AServlet.java
@@ -0,0 +1,24 @@
+package servlets;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.ServletException;
+
+import java.io.IOException;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/a.do")
+public class AServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        HttpSession session = request.getSession();
+
+        String sessionId = session.getId();
+        boolean sessionNayaHaiKya = session.isNew();
+
+        System.out.println("AServlet************SessionID: " + sessionId + " isNew: " + sessionNayaHaiKya);
+
+        request.getRequestDispatcher("pro.html").forward(request, response);
+    }
+}
