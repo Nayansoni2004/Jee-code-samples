afb5437dba45e29f1d267eb3443a4e22c78275be
NAYAN_SONI <nayansoni142004@gmail.com>
2024-10-10 23:22:41 +0530

    invalidate() method to manually close session

---
+++ 54_session_close/WEB-INF/src/servlets/AServlet.java
@@ -0,0 +1,28 @@
+package servlets;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.ServletException;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpSession;
+
+import java.util.Date;
+
+@WebServlet("/a.do")
+public class AServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        HttpSession session = request.getSession();
+
+        boolean sessionNayaHaiKya = session.isNew();
+        Date sessionCreationTime = new Date(session.getCreationTime());
+        Date sessionLastAccessedTime = new Date(session.getLastAccessedTime());
+
+        System.out.println("AServlet*******isNew: " + sessionNayaHaiKya + " SessionCreation Time: " + sessionCreationTime + " SessionLastAccessedTime: " + sessionLastAccessedTime);
+
+        request.getRequestDispatcher("pro.html").forward(request, response);
+    }
+}
---
+++ 54_session_close/WEB-INF/src/servlets/CServlet.java
@@ -0,0 +1,30 @@
+package servlets;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.ServletException;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpSession;
+
+import java.util.Date;
+
+@WebServlet("/c.do")
+public class CServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        HttpSession session = request.getSession();
+
+        boolean sessionNayaHaiKya = session.isNew();
+        Date sessionCreationTime = new Date(session.getCreationTime());
+        Date sessionLastAccessedTime = new Date(session.getLastAccessedTime());
+
+        session.invalidate();
+
+        System.out.println("CServlet******isNew: " + sessionNayaHaiKya + " SessionCreation Time: " + sessionCreationTime + " SessionLastAccessed Time: " + sessionLastAccessedTime);
+
+        request.getRequestDispatcher("next.html").forward(request, response);
+    }
+}
