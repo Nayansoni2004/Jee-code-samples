68b1e2834fc88beb2da419ed8e9dc5c269c9b33f
NAYAN_SONI <nayansoni142004@gmail.com>
2024-10-17 23:59:50 +0530

    controlled cookie timing in client's machine

---
+++ 59_cookie/WEB-INF/src/servlets/InfoServlet.java
@@ -0,0 +1,29 @@
+package servlets;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.ServletException;
+
+import java.io.IOException;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/info.do")
+public class InfoServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        HttpSession session = request.getSession();
+
+        // String cookies = request.getHeader("cookie");
+
+        Cookie[] cookies = request.getCookies();
+
+        for(Cookie cookie : cookies) {
+            System.out.println(cookie.getName() + " => " + cookie.getValue());
+        }
+
+        request.getRequestDispatcher("index.html").forward(request, response);
+    }
+}
